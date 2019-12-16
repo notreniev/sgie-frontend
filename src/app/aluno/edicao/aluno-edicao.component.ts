6905c4e92e307662b769473c771dab490958b613
everton-canez <everton-canez@procergs.rs.gov.br>
2019-12-16 17:07:31 -0300

    Alunos CRUD

--- src/app/aluno/edicao/aluno-edicao.component.ts
+++ src/app/aluno/edicao/aluno-edicao.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-aluno-edicao',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlunoEdicaoComponent implements OnInit {
 
-  constructor() { }
+  aluno: any = {}
+  ufs
+
+  constructor(private http: HttpClient) {
+  }
+
+  public getJSON(): Observable<any> {
+    return this.http.get("./assets/ufs.json");
+  }
+
+  buscaCep = async (numCep) => {
+    const result = await this.http.get<any>(`${environment.server_url}/cep/${numCep}`).toPromise()
+    try {
+      this.aluno.endereco = result.result[0].end
+      this.aluno.numero = result.result[0].numero
+      this.aluno.complemento = result.result[0].complemento2
+      this.aluno.bairro = result.result[0].bairro
+      this.aluno.cidade = result.result[0].cidade
+      this.aluno.uf = result.result[0].uf
+    } catch (error) {
+      console.log('error', error)
+    }    
+  }
+
+  save(aluno) {
+    console.log('aluno salvar', aluno)
+  }
 
   ngOnInit() {
+    this.getJSON().subscribe(data => {
+      this.ufs = data
+    })
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AlunoEdicaoComponent } from './aluno/edicao/aluno-edicao.component';
@@ -28,6 +29,7 @@ import { InstituicaoComponent } from './instituicao/lista/instituicao.component'
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
     NgbModule.forRoot()
   ],
   providers: [],
